86065854b71d9f2c5fb61ff2e90fc0e5d9d7d854
“Computarfant” <lennartbirk@gmail.com>
2023-04-20 22:35:02 +0200

    did not work

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -2,14 +2,14 @@ package mycrypt
 
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN")
 
-func Krypter(melding []rune, chiffer int) []rune {
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		indeks := sokIAlfabetet(melding[i], ALF_SEM03)
-		if indeks+chiffer >= len(ALF_SEM03) {
-			kryptertMelding[i] = ALF_SEM03[indeks+chiffer-len(ALF_SEM03)]
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
-			kryptertMelding[i] = ALF_SEM03[indeks+chiffer]
+			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
 	}
 	return kryptertMelding
