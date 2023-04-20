f444d6a367865f4c69fc40a050f0b9363d92bef8
“Computarfant” <lennartbirk@gmail.com>
2023-04-21 00:12:33 +0200

    negative slice

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -6,8 +6,12 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		if indeks == -1 {
+			kryptertMelding[i] = melding[i]
+		} else if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[(indeks+chiffer)%len(alphabet)]
+		} else if indeks+chiffer < 0 {
+			kryptertMelding[i] = alphabet[(indeks+chiffer+len(alphabet))%len(alphabet)]
 		} else {
 			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
